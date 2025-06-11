9e4d80fb6244bb8b6f4530a945b8f8a08c0bdf97
Thanigaivelan S <thanigaivelan.sekar@gmail.com>
2025-06-11 22:05:40 +0530

    adding common-repo-list.js

---
+++ common-repo-list.js
@@ -0,0 +1,12 @@
+[
+  {
+    "repo-name": "Dev-role-Springboot-proj",
+    "app-type": "springboot",
+    "branch": ["main", "dev", "feature"]
+  },
+  {
+    "repo-name": "dan-p81",
+    "app-type": "nginx",
+    "branch": ["main", "dev", "feature"]
+  }
+]
