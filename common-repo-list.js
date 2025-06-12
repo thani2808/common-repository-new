4861cc27e36a65be44a711c09086e2025530b32a
Thanigaivelan S <thanigaivelan.sekar@gmail.com>
2025-06-12 06:58:24 +0530

    github filepath added

--- common-repo-list.js
+++ common-repo-list.js
@@ -2,11 +2,11 @@
   {
     "repo-name": "Dev-role-Springboot-proj",
     "app-type": "springboot",
-    "branch": ["main", "dev", "feature"]
+    "git-url": "git@github.com:thani2808/Dev-role-Springboot-proj.git"	  
   },
   {
     "repo-name": "dan-p81",
     "app-type": "nginx",
-    "branch": ["main", "dev", "feature"]
+    "git-url": "git@github.com:thani2808/dan-p81.git"	  
   }
 ]
