fb284166572dfb493c3bfda14ab87c82d3734829
Thanigaivelan S <thanigaivelan.sekar@gmail.com>
2025-06-12 07:28:35 +0530

    modified common-repo

--- common-repo-list.js
+++ common-repo-list.js
@@ -2,11 +2,17 @@
   {
     "repo-name": "Dev-role-Springboot-proj",
     "app-type": "springboot",
-    "git-url": "git@github.com:thani2808/Dev-role-Springboot-proj.git"	  
+    "git-url": "git@github.com:thani2808/Dev-role-Springboot-proj.git"
+    "DOCKERHUB_USERNAME": "thanigai2808"
+    "HOST_PORT": "9004"
+    "GIT_CREDENTIALS_ID": "private-key-jenkins"	  
   },
   {
     "repo-name": "dan-p81",
     "app-type": "nginx",
     "git-url": "git@github.com:thani2808/dan-p81.git"	  
+    "DOCKERHUB_USERNAME": "thanigai2808"
+    "HOST_PORT": "9004"
+    "GIT_CREDENTIALS_ID": "private-key-jenkins"
   }
 ]
