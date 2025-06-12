65eddc93005342c477f93e95279bfe445df9ee9e
Thanigaivelan S <thanigaivelan.sekar@gmail.com>
2025-06-12 08:02:07 +0530

    changed the file

--- common-repo-list.js
+++ common-repo-list.js
@@ -3,16 +3,16 @@
     "repo-name": "Dev-role-Springboot-proj",
     "app-type": "springboot",
     "git-url": "git@github.com:thani2808/Dev-role-Springboot-proj.git",
-    "DOCKERHUB_USERNAME": "thanigai2808",
-    "HOST_PORT": "9004",
-    "GIT_CREDENTIALS_ID": "private-key-jenkins"	  
+    "dockerhub_username": "thanigai2808",
+    "host_port": "9004",
+    "git_credentials_id": "private-key-jenkins"
   },
   {
     "repo-name": "dan-p81",
     "app-type": "nginx",
-    "git-url": "git@github.com:thani2808/dan-p81.git",	  
-    "DOCKERHUB_USERNAME": "thanigai2808",
-    "HOST_PORT": "9004",
-    "GIT_CREDENTIALS_ID": "private-key-jenkins"
+    "git-url": "git@github.com:thani2808/dan-p81.git",
+    "dockerhub_username": "thanigai2808",
+    "host_port": "9004",
+    "git_credentials_id": "private-key-jenkins"
   }
 ]
