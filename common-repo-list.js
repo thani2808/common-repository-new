8add0e058835b1333c2c424e2510e138a14be629
Thanigaivelan S <thanigaivelan.sekar@gmail.com>
2025-06-14 13:24:40 +0530

    adding start-containers file

--- common-repo-list.js
+++ common-repo-list.js
@@ -8,6 +8,14 @@
     "git_credentials_id": "private-key-jenkins"
   },
   {
+    "repo-name": "Dev-role-Springboot-proj",
+    "app-type": "springboot",
+    "git-url": "git@github.com:thani2808/Dev-role-Springboot-proj.git",
+    "dockerhub_username": "thanigai2808",
+    "host_port": "8761",
+    "git_credentials_id": "private-key-jenkins"
+  },
+  {
     "repo-name": "dan-p81",
     "app-type": "nginx",
     "git-url": "git@github.com:thani2808/dan-p81.git",
