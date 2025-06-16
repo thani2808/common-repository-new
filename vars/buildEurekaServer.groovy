787057700ba50dfd832322f1e74d009789a36eae
Thanigaivelan S <thanigaivelan.sekar@gmail.com>
2025-06-16 18:40:51 +0530

    modified buildEurekaServer.groogy

--- vars/buildEurekaServer.groovy
+++ vars/buildEurekaServer.groovy
@@ -1,5 +1,5 @@
-def call() {
-    dir('common-repository-new/eureka-discovery-server') {
+def call(String projectDir) {
+    dir(projectDir) {
         sh '''
             echo "🧪 Building Eureka JAR..."
             mvn clean package -DskipTests
