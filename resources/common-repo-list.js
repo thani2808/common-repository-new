a83e09c9a14dbef1975d0619661dc8414106d417
Thanigaivelan S <thanigaivelan.sekar@gmail.com>
2025-06-16 12:16:19 +0530

    adding commonConfig.groovy

---
+++ resources/common-repo-list.js
@@ -0,0 +1,34 @@
+[
+  {
+    "repo-name": "Dev-role-Springboot-proj",
+    "app-type": "springboot",
+    "git-url": "git@github.com:thani2808/Dev-role-Springboot-proj.git",
+    "dockerhub_username": "thanigai2808",
+    "host_port": "9004",
+    "git_credentials_id": "private-key-jenkins"
+  },
+  {
+    "repo-name": "eureka-discovery-server",
+    "app-type": "springboot",
+    "git-url": "git@github.com:thani2808/Dev-role-Springboot-proj.git",
+    "dockerhub_username": "thanigai2808",
+    "host_port": "8761",
+    "git_credentials_id": "private-key-jenkins"
+  },
+  {
+    "repo-name": "dan-p81",
+    "app-type": "nginx",
+    "git-url": "git@github.com:thani2808/dan-p81.git",
+    "dockerhub_username": "thanigai2808",
+    "host_port": "9004",
+    "git_credentials_id": "private-key-jenkins"
+  },
+  {
+    "repo-name": "hello-world-springboot",
+    "app-type": "springboot",
+    "git-url": "git@github.com:thani2808/hello-world-springboot.git",
+    "dockerhub_username": "thanigai2808",
+    "host_port": "9004",
+    "git_credentials_id": "private-key-jenkins"
+  }
+]
