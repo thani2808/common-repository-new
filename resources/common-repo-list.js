d146321e2c52bbcbd4607d8f1e0b96ff50af0e31
Thanigaivelan S <thanigaivelan.sekar@gmail.com>
2025-06-17 19:04:04 +0530

    changed common-repo-list.js

--- resources/common-repo-list.js
+++ resources/common-repo-list.js
@@ -1,38 +1,50 @@
-[
-  {
-    "repo-name": "Dev-role-Springboot-proj",
-    "app-type": "springboot",
-    "project_dir": ".",
-    "git-url": "git@github.com:thani2808/Dev-role-Springboot-proj.git",
-    "dockerhub_username": "thanigai2808",
-    "host_port": "9004",
-    "git_credentials_id": "private-key-jenkins"
-  },
-  {
-    "repo-name": "eureka-discovery-server",
-    "app-type": "eureka",
-    "project_dir": ".",
-    "git-url": "git@github.com:thani2808/common-repository-new.git",
-    "dockerhub_username": "thanigai2808",
-    "host_port": "8761",
-    "git_credentials_id": "private-key-jenkins"
-  },
-  {
-    "repo-name": "dan-p81",
-    "app-type": "nginx",
-    "project_dir": ".",
-    "git-url": "git@github.com:thani2808/dan-p81.git",
-    "dockerhub_username": "thanigai2808",
-    "host_port": "9004",
-    "git_credentials_id": "private-key-jenkins"
-  },
-  {
-    "repo-name": "hello-world-springboot",
-    "app-type": "springboot",
-    "project_dir": ".",
-    "git-url": "git@github.com:thani2808/hello-world-springboot.git",
-    "dockerhub_username": "thanigai2808",
-    "host_port": "9004",
-    "git_credentials_id": "private-key-jenkins"
-  }
-]
+{
+  "springboot": [
+    {
+      "repo-name": "Dev-role-Springboot-proj",
+      "project_dir": ".",
+      "git-url": "git@github.com:thani2808/Dev-role-Springboot-proj.git",
+      "dockerhub_username": "thanigai2808",
+      "host_port": "9004",
+      "git_credentials_id": "private-key-jenkins"
+    },
+    {
+      "repo-name": "hello-world-springboot",
+      "project_dir": ".",
+      "git-url": "git@github.com:thani2808/hello-world-springboot.git",
+      "dockerhub_username": "thanigai2808",
+      "host_port": "9004",
+      "git_credentials_id": "private-key-jenkins"
+    }
+  ],
+  "eureka": [
+    {
+      "repo-name": "eureka-discovery-server",
+      "project_dir": ".",
+      "git-url": "git@github.com:thani2808/common-repository-new.git",
+      "dockerhub_username": "thanigai2808",
+      "host_port": "8761",
+      "git_credentials_id": "private-key-jenkins"
+    }
+  ],
+  "nginx": [
+    {
+      "repo-name": "dan-p81",
+      "project_dir": ".",
+      "git-url": "git@github.com:thani2808/dan-p81.git",
+      "dockerhub_username": "thanigai2808",
+      "host_port": "9005",
+      "git_credentials_id": "private-key-jenkins"
+    }
+  ],
+  "nodejs": [
+    {
+      "repo-name": "sample-project-future",
+      "project_dir": ".",
+      "git-url": "git@github.com:thani2808/some-node-project.git",
+      "dockerhub_username": "thanigai2808",
+      "host_port": "9006",
+      "git_credentials_id": "private-key-jenkins"
+    }
+  ]
+}
