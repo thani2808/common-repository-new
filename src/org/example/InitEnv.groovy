617f5c98eb8b834ee6e18cdf579e47ec53334b5b
Thanigaivelan S <thanigaivelan.sekar@gmail.com>
2025-06-17 15:15:06 +0530

    initenv changed

--- src/org/example/InitEnv.groovy
+++ src/org/example/InitEnv.groovy
@@ -10,6 +10,10 @@ class InitEnv implements Serializable {
     }
 
     void init(String repoName) {
+        // Always set PROJECT_DIR
+        steps.env.PROJECT_DIR = repoName
+
+        // Load and parse the repo config list
         def jsonText = steps.libraryResource('common-repo-list.js')
         def parsedList = new JsonSlurper().parseText(jsonText)
         def matchedConfig = parsedList.find { it['repo-name'] == repoName }
@@ -18,10 +22,11 @@ class InitEnv implements Serializable {
             steps.error "❌ No config found for repo: ${repoName}"
         }
 
-        def config = new CommonConfig(matchedConfig)
-        steps.env.APP_TYPE = config.appType
-        steps.env.IMAGE_NAME = config.imageName
-        steps.env.CONTAINER_NAME = config.containerName
-        steps.env.DOCKER_PORT = config.dockerPort
+        // Dynamically set env vars from config
+        steps.env.APP_TYPE       = matchedConfig['app-type'] ?: 'unknown'
+        steps.env.IMAGE_NAME     = matchedConfig['image-name'] ?: "${repoName.toLowerCase()}-image"
+        steps.env.CONTAINER_NAME = matchedConfig['container-name'] ?: "${repoName.toLowerCase()}-container"
+        steps.env.DOCKER_PORT    = matchedConfig['docker-port']?.toString() ?: "8080"
+        steps.env.IS_EUREKA      = (matchedConfig['is-eureka']?.toString() ?: "false").toLowerCase()
     }
 }
