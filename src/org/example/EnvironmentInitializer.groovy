1b12f96232ed047528acb83a433b81885221f6f1
Thanigaivelan S <thanigaivelan.sekar@gmail.com>
2025-06-19 11:45:02 +0530

    modified

--- src/org/example/EnvironmentInitializer.groovy
+++ src/org/example/EnvironmentInitializer.groovy
@@ -8,12 +8,16 @@ class EnvironmentInitializer implements Serializable {
     }
 
     void initialize() {
+        steps.echo "🛠️ [EnvironmentInitializer] initialize() called"
+
         def envLoader = new EnvLoader(steps)
         def envVars = envLoader.load()
 
-        steps.echo "🔍 Loaded Env Vars: ${envVars.inspect()}"
+        steps.echo "🔍 [EnvironmentInitializer] envVars.inspect() = ${envVars.inspect()}"
 
         steps.env.APP_TYPE            = envVars.APP_TYPE ?: 'springboot'
+        steps.echo "➡️ [EnvironmentInitializer] Set APP_TYPE = '${steps.env.APP_TYPE}'"
+
         steps.env.IMAGE_NAME          = envVars.IMAGE_NAME
         steps.env.CONTAINER_NAME      = envVars.CONTAINER_NAME
         steps.env.HOST_PORT           = envVars.HOST_PORT
@@ -21,5 +25,7 @@ class EnvironmentInitializer implements Serializable {
         steps.env.DOCKERHUB_USERNAME  = envVars.DOCKERHUB_USERNAME
         steps.env.GIT_CREDENTIALS_ID  = envVars.GIT_CREDENTIALS_ID
         steps.env.GIT_URL             = envVars.GIT_URL
+
+        steps.echo "✅ [EnvironmentInitializer] Completed"
     }
 }
